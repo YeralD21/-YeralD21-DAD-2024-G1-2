9fbbfecbe412be672e348b295b56df7396e41f81
YeralD21 <yesarbot1@gmail.com>
2024-04-11 10:33:36 -0500

    ModifiedPedidoDetalle

--- ms-pedido/src/main/java/com/example/mspedido/entity/Pedido.java
+++ ms-pedido/src/main/java/com/example/mspedido/entity/Pedido.java
@@ -1,15 +1,23 @@
 package com.example.mspedido.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.List;
+
 @Entity
 @Data
 public class Pedido {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String nombre;
+    private String serie;
+    private String numero;
+    private String descripcion;
+    private Integer clienteId;
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "venta_id")
+    private List<PedidoDetalle> detalle;
 }
